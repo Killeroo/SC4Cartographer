58f832d6146fe3044504ef2cb9fcfd87538b0aaf
Matthew Carney <matthewcarney64@gmail.com>
2020-12-23 12:18:59 +0000

    Added comments to UpdateChecker.cs

--- SC4Cartographer/UpdateChecker.cs
+++ SC4Cartographer/UpdateChecker.cs
@@ -5,15 +5,28 @@ namespace SC4CartographerUI
 {
     class UpdateChecker
     {
+        /// <summary>
+        /// Get latest info about latest release on github via an async download string call
+        /// </summary>
+        /// <param name="handler">callback to be fired when data is downloaded</param>
         public static void GetLatestReleaseInfoAsync(DownloadStringCompletedEventHandler handler)
         {
+            // Setup web client
             WebClient client = new WebClient();
             ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
             client.Headers["User-Agent"] = "SC4Cartographer (version_check)";
+
+            // Add callback 
             client.DownloadStringCompleted += (sender, args) => handler(sender, args);
+            
+            // Kick off async download
             client.DownloadStringAsync(new Uri(@"http://api.github.com/repos/killeroo/sc4cartographer/releases/latest"));
         }
 
+        /// <summary>
+        /// Gets info about the latest release from github via a blocking web call
+        /// </summary>
+        /// <returns>Info about the release</returns>
         public static UpdateInfo GetLatestReleaseInfo()
         {
             WebClient client = new WebClient();
